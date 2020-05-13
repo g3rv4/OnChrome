d2b038a227d934a40e300633afe977452dcc7040
reb00tz <20411812+reb00tz@users.noreply.github.com>
2020-05-13 23:02:24 +0800

    force new instance of chrome with selected profile
    force chrome to run in new instance with selected profile

--- app/extensionendpoint/openChrome_darwin.go
+++ app/extensionendpoint/openChrome_darwin.go
@@ -2,7 +2,7 @@ package extensionendpoint
 
 import "os/exec"
 
-func openChrome(url string) error {
-	cmd := exec.Command("open", "-a", "Google Chrome", url)
+func openChrome(profile string, url string) error {
+    cmd := exec.Command("open", "-n", "-a", "Google Chrome", url, "--profile-directory='".join(profile).join("'"))
 	return cmd.Run()
 }
