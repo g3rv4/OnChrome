eac311f48c54d3e1ffa776bfe5d2d1c6e3bf2eec
Max Gerlach <max@ada.support>
2020-05-26 19:31:26 -0400

    Allow exclusion matches

--- extension/background.js
+++ extension/background.js
@@ -7,30 +7,42 @@ function onError(error) {
 }
 
 var currentProfile;
-const blockUrl = function (requestDetails) {
-    return new Promise(function(resolve, reject) {
-        browser.runtime.sendNativeMessage(
-            "me.onchro",
-            { url: requestDetails.url, profile: currentProfile }).then(() => browser.tabs.remove(requestDetails.tabId), onError);
-        resolve({cancel: true});
-      });
+const blockUrl = function (exclusions) {
+    return function(requestDetails){
+        return new Promise(function(resolve, reject) {
+            var resolved = false;
+            for(exclusion of exclusions){
+                if(requestDetails.url.match(new RegExp(exclusion))) {
+                    resolve({cancel:false});
+                    resolved = true;
+                }
+            }
+            if (!resolved){
+                browser.runtime.sendNativeMessage(
+                    "me.onchro",
+                    { url: requestDetails.url, profile: currentProfile }).then(() => browser.tabs.remove(requestDetails.tabId), onError);
+                resolve({cancel: true});
+            }
+        });
+    }
 }
 
-function registerUrls(urls, profile) {
+function registerUrls(urls, exclusions, profile) {
     currentProfile = profile;
     if (urls) {
         urls = JSON.parse(urls);
+        exclusions = JSON.parse(exclusions);
         if (urls && urls.length) {
-            browser.webRequest.onBeforeRequest.addListener(blockUrl, { urls: urls, types: ["main_frame"] }, ["blocking"])
+            browser.webRequest.onBeforeRequest.addListener(blockUrl(exclusions), { urls: urls, types: ["main_frame"] }, ["blocking"])
         }
     }
 }
 
-chrome.storage.sync.get(["urls"], res => registerUrls(res.urls))
+chrome.storage.sync.get(["urls", "exclusions"], res => registerUrls(res.urls, res.exclusions))
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.type === "urlsUpdated") {
         browser.webRequest.onBeforeRequest.removeListener(blockUrl)
-        chrome.storage.sync.get(["urls", "profile"], res => registerUrls(res.urls, res.profile))
+        chrome.storage.sync.get(["urls", "profile", exclusions], res => registerUrls(res.urls, res.exclusions, res.profile))
     }
 });
