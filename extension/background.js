1decf93ff22bdaf2fd9331770f0f4489bd8b95aa
Gervasio Marchand <github@gervas.io>
2020-05-31 17:10:06 -0300

    Minor tweaks :)

--- extension/background.js
+++ extension/background.js
@@ -6,43 +6,36 @@ function onError(error) {
     });
 }
 
-var currentProfile;
-const blockUrl = function (exclusions) {
-    return function(requestDetails){
-        return new Promise(function(resolve, reject) {
-            var resolved = false;
-            for(exclusion of exclusions){
-                if(requestDetails.url.match(new RegExp(exclusion))) {
-                    resolve({cancel:false});
-                    resolved = true;
-                }
-            }
-            if (!resolved){
-                browser.runtime.sendNativeMessage(
-                    "me.onchro",
-                    { url: requestDetails.url, profile: currentProfile }).then(() => browser.tabs.remove(requestDetails.tabId), onError);
-                resolve({cancel: true});
-            }
-        });
-    }
+var currentProfile, currentExclusions;
+const blockUrl = function (requestDetails) {
+    return new Promise(function (resolve, reject) {
+        if (currentExclusions.find(r => requestDetails.url.match(r))) {
+            resolve({ cancel: false });
+            return
+        }
+        browser.runtime.sendNativeMessage(
+            "me.onchro",
+            { url: requestDetails.url, profile: currentProfile }).then(() => browser.tabs.remove(requestDetails.tabId), onError);
+        resolve({ cancel: true });
+    });
 }
 
 function registerUrls(urls, exclusions, profile) {
     currentProfile = profile;
     if (urls) {
         urls = JSON.parse(urls);
-        exclusions = JSON.parse(exclusions);
+        currentExclusions = exclusions ? JSON.parse(exclusions).map(s => new RegExp(s)) : [];
         if (urls && urls.length) {
-            browser.webRequest.onBeforeRequest.addListener(blockUrl(exclusions), { urls: urls, types: ["main_frame"] }, ["blocking"])
+            browser.webRequest.onBeforeRequest.addListener(blockUrl, { urls: urls, types: ["main_frame"] }, ["blocking"])
         }
     }
 }
 
-chrome.storage.sync.get(["urls", "exclusions"], res => registerUrls(res.urls, res.exclusions))
+chrome.storage.sync.get(["urls", "profile", "exclusions"], res => registerUrls(res.urls, res.exclusions, res.profile))
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.type === "urlsUpdated") {
         browser.webRequest.onBeforeRequest.removeListener(blockUrl)
-        chrome.storage.sync.get(["urls", "profile", exclusions], res => registerUrls(res.urls, res.exclusions, res.profile))
+        chrome.storage.sync.get(["urls", "profile", "exclusions"], res => registerUrls(res.urls, res.exclusions, res.profile))
     }
 });
