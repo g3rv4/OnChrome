d1b6cff8e3a8309cf931cbb21e220b8d91c12c03
Gervasio Marchand <github@gervas.io>
2020-06-27 00:55:03 -0300

    another one that's not needed

--- extension/inject-version.js
+++
@@ -1,8 +0,0 @@
-var actualCode = `
-window.OnChromeVersion = '${browser.runtime.getManifest().version}';
-`;
-
-var script = document.createElement('script');
-script.textContent = actualCode;
-(document.head || document.documentElement).appendChild(script);
-script.remove();
