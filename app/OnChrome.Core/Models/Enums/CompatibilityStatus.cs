87b1fc89660da25143a0a15461efcd9a1b2d5d1b
Gervasio Marchand <github@gervas.io>
2020-06-27 00:52:59 -0300

    No longer need these

--- app/OnChrome.Core/Models/Enums/CompatibilityStatus.cs
+++ app/OnChrome.Core/Models/Enums/CompatibilityStatus.cs
@@ -10,46 +10,4 @@ namespace OnChrome.Core.Models.Enums
         ExtensionNeedsUpdate,
         Ok
     }
-
-    public static class CompatibilityStatusExtensions
-    {
-        public static bool IsAppOk(this CompatibilityStatus status) =>
-            status != CompatibilityStatus.AppNeedsUpdate &&
-            status != CompatibilityStatus.Unknown;
-
-        public static bool IsExtensionOk(this CompatibilityStatus status) =>
-            status == CompatibilityStatus.Ok ||
-            status == CompatibilityStatus.AppNeedsUpdate;
-        
-        public static string GetAppDetails(this CompatibilityStatus status)
-        {
-            switch (status)
-            {
-                case CompatibilityStatus.MissingExtension:
-                case CompatibilityStatus.Ok:
-                case CompatibilityStatus.ExtensionNeedsUpdate:
-                    return "You are running the application successfully.";
-                case CompatibilityStatus.AppNeedsUpdate:
-                    return "You need to update the application.";
-                default:
-                    return "Could not determine app status.";
-            }
-        }
-
-        public static string GetExtensionDetails(this CompatibilityStatus status)
-        {
-            switch (status)
-            {
-                case CompatibilityStatus.AppNeedsUpdate:
-                case CompatibilityStatus.Ok:
-                    return "You are running the extension successfully.";
-                case CompatibilityStatus.MissingExtension:
-                    return "You need to install the extension.";
-                case CompatibilityStatus.ExtensionNeedsUpdate:
-                    return "You need to update the extension.";
-                default:
-                    return "Could not determine extension status.";
-            }
-        }
-    }
 }
